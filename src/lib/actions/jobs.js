9e9e3da4e567c79732b93165176acd839f79c6a3
Kayes Mahmud <mahmudkayes30@gmail.com>
2026-06-19 19:35:22 +0600

    Added the API Intigration for Company profiles

--- src/lib/actions/jobs.js
+++ src/lib/actions/jobs.js
@@ -1,14 +1,22 @@
-"use server";
+'use server'
 
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+import { serverMutation } from "../core/server";
+
+
 export const createJob = async (newJobData) => {
-  const res = await fetch(`${baseUrl}/api/jobs`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newJobData),
-  });
+    return serverMutation('/api/jobs', newJobData);
+}
 
-  return res.json();
-};
+// const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+// export const createJob = async (newJobData) => {
+//     const res = await fetch(`${baseUrl}/api/jobs`, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(newJobData),
+//     });
+
+//     return res.json();
+// }
